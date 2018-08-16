0074469df27d450cc0a75c9a1dae16c81e9f751c
Jason Brownell <jason.michael.brownell@outlook.com>
2018-08-15 21:36:58 -0400

    Injection of messaging service into the Hero service

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,14 +4,20 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes';  // URL to web api
+
   // constructor() { }
 
-  constructor(private messageService: MessageService) { }
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService) { }
 
   /*getHeroes(): Hero[] {
     return HEROES;
@@ -31,5 +37,10 @@ export class HeroService {
     // TODO: send the message _after_ fetching the hero
     this.messageService.add(`HeroService: fetched hero id=${id}`);
     return of(HEROES.find(hero => hero.id === id));
+  }
+
+  /** Log a HeroService message with the MessageService */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 }
