d7b97a1373dbaba4889be825a1bf0492fbc4067f
Jason Brownell <jason.michael.brownell@outlook.com>
2018-08-15 21:43:02 -0400

    Error-Handing Function and some minor tweaks + cleanup

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -6,6 +6,8 @@ import { MessageService } from './message.service';
 
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
+import { catchError, map, tap } from 'rxjs/operators';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -19,18 +21,12 @@ export class HeroService {
     private http: HttpClient,
     private messageService: MessageService) { }
 
-  /*getHeroes(): Hero[] {
-    return HEROES;
-  }*/
-
-  /*getHeroes(): Observable<Hero[]> {
-    return of(HEROES);
-  }*/
-
-  getHeroes(): Observable<Hero[]> {
-    // TODO: send the message _after_ fetching the heroes
-    this.messageService.add('HeroService: fetched heroes');
-    return of(HEROES);
+  /** GET heroes from the server */
+  getHeroes (): Observable<Hero[]> {
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        catchError(this.handleError('getHeroes', []))
+      );
   }
 
   getHero(id: number): Observable<Hero> {
@@ -42,5 +38,25 @@ export class HeroService {
   /** Log a HeroService message with the MessageService */
   private log(message: string) {
     this.messageService.add(`HeroService: ${message}`);
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 }
